20a459e35cf75b5f223a2d00ea2431263a283292
Khalid M. Sheet <khalid.m.sheet@gmail.com>
2024-10-23 17:41:30 +0300

    FEAT: :construction: Getting the Calendar view to work.

--- README.md
+++ README.md
@@ -1,15 +1,5 @@
 # event-management-dashboard
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
-
 ## Project Setup
 
 ```sh
@@ -33,3 +23,16 @@ npm run build
 ```sh
 npm run lint
 ```
+
+---
+
+## Project Details
+
+### Tools
+
+- VueJS
+- Vuex (State Management)
+
+### UI Libraries
+
+- VueCal
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import 'vue-cal/dist/vuecal.css'
+import VueCal from 'vue-cal'
+import { ref } from 'vue';
+
+const events = ref([
+  {
+    start: '2024-10-16 10:30',
+    end: '2024-10-16 11:30',
+    title: 'Doctor appointment',
+    content: '<i class="icon material-icons">local_hospital</i>',
+    class: 'bg-red-400'
+  }])
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="h-screen">
+    <VueCal
+      hide-view-selector
+      active-view="month"
+      :events="events"
+     />
   </main>
 </template>
